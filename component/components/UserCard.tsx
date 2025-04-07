747e50ad7447d70a6c580ff797ebfdfedc90b79b
Draisen2711 <146686646+Draisen2711@users.noreply.github.com>
2025-04-07 19:22:48 +0800

    Add files via upload

---
+++ component/components/UserCard.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import {View, Text, Image, StyleSheet} from 'react-native';
+
+const UserCard = ({user}) => {
+  return (
+    <View style={styles.card}>
+      <Image source={{uri: user.avatar}} style={styles.avatar} />
+      <View style={styles.userInfo}>
+        <Text style={styles.name}>
+          <Text style={styles.bold}>{user.last_name}</Text> {user.first_name}
+        </Text>
+        <Text style={styles.email}>{user.email}</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 15,
+    backgroundColor: '#fff',
+    borderWidth: 0.4,
+    borderRadius: 20,
+    marginBottom: 25,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 3,
+  },
+  avatar: {
+    width: 100,
+    height: 100,
+    borderRadius: 10,
+    marginRight: 15,
+  },
+  userInfo: {
+    flex: 1,
+  },
+  name: {
+    fontSize: 16,
+  },
+  bold: {
+    fontWeight: 'bold',
+  },
+  email: {
+    color: 'gray',
+  },
+});
+
+export default UserCard;
